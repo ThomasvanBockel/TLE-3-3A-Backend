16348004820874da62056ebe9afdab7ae663c2f5
Thomas van Bockel <Tnvanbockel@gmail.com>
2026-03-04 15:11:39 +0100

    made the post login  backend

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,22 +4,34 @@ import bcrypt from "bcrypt"
 
 
 const userRouter = express.Router()
-userRouter.get("/", (req, res) => {
-
+userRouter.get("/", async (req, res) => {
+    try {
+        const user = await User.find({email: req.body.email})
+        if (!user) {
+            return res.status(404).json("user not found")
+        }
+        res.json(user)
+    } catch (e) {
+        console.log(e)
+    }
     res.json("hello world")
 })
 
-userRouter.post("/", async (req, res) => {
+userRouter.post("/register", async (req, res) => {
     try {
+        if (!req.body.name || !req.body.email || !req.body.password) {
+            return res.status(400).json("emty erea's")
+        }
+        // hashing password
         const password = req.body.password
         const SALT_ROUNDS = 10
         const passwordHashed = await bcrypt.hash(password, SALT_ROUNDS);
-        if (!req.body.name || !req.body.email || !req.body.password) {
-            res.status(400).json("emty erea's")
-        }
 
+        // if email exist give an error
         const exists = await User.findOne({email: req.body.email})
-        if (exists) return res.status(400).json({message: "Email already exists"})
+        if (exists) {
+            return res.status(400).json({message: "Email already exists"})
+        }
 
         const user = new User({
             name: req.body.name,
@@ -32,7 +44,31 @@ userRouter.post("/", async (req, res) => {
 
         })
         await user.save()
-        res.status(201)
+        res.status(201).json("account created")
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+userRouter.post("/login", async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).json("empty erea's")
+        }
+        // finding the user with password
+        const user = await User.findOne({email: req.body.email})
+        if (!user) {
+            return res.status(404).json("user not found")
+        }
+        // unhashing password
+        const password = req.body.password
+
+        const is_match = await bcrypt.compare(password, user.password_hash);
+        if (!is_match) {
+            return res.status(401).json("password is not correct")
+        }
+
+        res.status(200).json(user)
     } catch (e) {
         console.log(e)
     }
