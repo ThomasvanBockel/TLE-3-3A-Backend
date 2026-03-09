f9ebedc980e4afbd41af3c46b2ec846f26d82d96
1073500 <1073500@hr.nl>
2026-03-09 11:46:44 +0100

    Inquiry type

--- routes/routeRouter.js
+++ routes/routeRouter.js
@@ -3,10 +3,12 @@ import userRouter from "./userRouter.js"
 import contentItem from "../models/ContentItem.js";
 import contentItemRouter from "./contentItemRouter.js";
 import InquiryRouter from "./inquiryRouter.js";
+import InquiryTypeRouter from "./inquiryTypeRouter.js";
 
 const router = express.Router()
 router.use("/user", userRouter)
 router.use("/inquiry", InquiryRouter)
 router.use("/api/content-items/", contentItemRouter)
+router.use("/inquiry-types/", InquiryTypeRouter)
 
 export default router
