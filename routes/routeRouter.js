174acd75c09beb48f801c76308f0c04a60de2365
Thomas van Bockel <Tnvanbockel@gmail.com>
2026-03-10 11:23:30 +0100

    added /api infront of all the routes

--- routes/routeRouter.js
+++ routes/routeRouter.js
@@ -11,12 +11,10 @@ import documentType from "./documentTypeRouter.js";
 const router = express.Router()
 router.use("/api/user", userRouter)
 router.use("/api/inquiry", InquiryRouter)
-router.use("/user", userRouter)
-router.use("/inquiry", inquiryRouter)
-router.use("/documents/", documentRouter)
+router.use("/api/documents/", documentRouter)
 router.use("/api/content-items/", contentItemRouter)
 router.use("/api/categories", categoryRouter)
-router.use("/inquiry-types/", inquiryTypeRouter)
-router.use("/document-types/", documentType)
+router.use("/api/inquiry-types/", inquiryTypeRouter)
+router.use("/api/document-types/", documentType)
 
 export default router
